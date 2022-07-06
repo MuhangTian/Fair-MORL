b459d867ef488397ade81d547b68d9c778bb9090
Tony_Tian_1122 <tonytian@stayafloat.io>
2022-07-05 23:21:54 -0400

    Begin resource allocation env

--- scratch.py
+++ scratch.py
@@ -1,19 +1,8 @@
-from matplotlib.pyplot import fill
-from Experiments.Fair_Taxi_Bandit import Fair_Taxi_Bandit
 import numpy as np
 
-# env = Fair_Taxi_Bandit(num_locs=5, max_mean=100, 
-#                                     min_mean=10, sd=0, 
-#                                     center_mean=20, max_diff=0,
-#                                     output_path='Bandit_Qlearning/Q_learning_run_')
-# env.step(1)
-# print(env.rewards)
-# env.step(2)
-# print(env.rewards)
-# print(env.rewards)
-# env.step(1)
-# print(env.rewards)
-
-arr = np.array([[1,2,3], [1,2,3], [1,2,3]])
-arr = [np.log(arr[1]), np.log(arr)
-print(np.sum(np.log(arr), axis=1))
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+print(arr)
+print(arr[0][2])
+arr = np.array(arr)
+print(arr)
+print(arr[0,2])
