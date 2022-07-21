1513d59fb5e064e42a80c28e8b4a165b09213e24
Tony_Tian_1122 <tonytian@stayafloat.io>
2022-07-21 15:38:45 -0400

    to cluster

--- Experiments/nsw2_5.sh
+++ Experiments/nsw2_5.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --ntasks=1                    
-#SBATCH --cpus-per-task=48            
-#SBATCH --mem=50g
+#SBATCH --cpus-per-task=24          
+#SBATCH --mem=25g
 #SBATCH --time=4-00:00:00               # Time limit hrs:min:sec
 #SBATCH --output=%J.out
 #SBATCH --error=%J.err
